9b9b25507f82c7daf877e8672644e924537c5328
Andy Burke <aburke@bitflood.org>
2020-07-06 23:17:09 -0700

    feature: add TAP output option

--- baretest.js
+++ baretest.js
@@ -7,7 +7,11 @@ module.exports = function(headline) {
     after = [],
     only = []
 
-  let quiet = true
+  let bail = true,
+    quiet = true,
+    tap = false,
+    count = 0,
+    passed = true
 
   function self(name, fn) {
     suite.push({ name: name, fn: fn })
@@ -25,58 +29,113 @@ module.exports = function(headline) {
     suite.push({ group: name })
   }
 
-  self.quiet = function(_quiet) {
-    quiet = _quiet
-  }
+  self.bail = function(_bail) { bail = _bail }
+  self.quiet = function(_quiet) { quiet = _quiet }
+  self.tap = function(_tap) { tap = _tap }
 
   self.run = async function() {
     const tests = only[0] ? only : suite
 
-    rgb.cyan(headline + '\n')
+    if (tap) {
+      console.log(`# ${ headline }`)
+      console.log(`1..${ tests.filter(test => typeof test.group === 'undefined').length}`)
+    }
+    else {
+      rgb.cyan(headline + '\n')
+    }
 
     for (const test of tests) {
       if (test.group) {
-        rgb.cyan(`${ quiet ? '\n' : '' }  ${ test.group }${ quiet ? ' ' : '\n' }`)
+        emit(test)
         continue
       }
 
       try {
+        ++count;
         for (const fn of before) await fn()
         await test.fn()
-        if (quiet) {
-          rgb.gray('• ')
-        }
-        else {
-          rgb.gray(`    ${ test.name } `)
-        }
-
+        emit({
+          count,
+          test,
+          passed: true
+        });
       } catch(e) {
         for (const fn of after) await fn()
-        if (quiet) {
-          rgb.red(`\n\n! ${test.name} \n\n`)
-        }
-        else {
-          rgb.redln('✘')
-        }
-        prettyError(e)
-        return false
-      }
 
-      if (!quiet) {
-        rgb.greenln('✓')
+        emit({
+          count,
+          test,
+          failed: true,
+          e
+        })
+
+        if (bail) {
+          if (tap) {
+            console.log('Bail out!')
+          }
+          return false
+        }
+
+        passed = false
       }
     }
 
     for (const fn of after) await fn()
-    rgb.greenln(`\n✓ ${ tests.filter( test => typeof test.group === 'undefined' ).length }`)
+    if (!tap) {
+      rgb.greenln(`\n✓ ${ tests.filter( test => typeof test.group === 'undefined' ).length }`)
+    }
     console.info('\n')
-    return true
+    return passed
+  }
+
+  function emit(result) {
+    if (result.group) {
+      if (tap) {
+        console.log( `#   ${ result.group }` )
+      }
+      else if (quiet) {
+        rgb.cyan(`\n  ${ result.group } `)
+      }
+      else {
+        rgb.cyan(`  ${ result.group }\n`)
+      }
+    }
+    else if (result.passed) {
+      if (tap) {
+        console.log(`ok ${ result.count } ${ result.test.name }`)
+      }
+      else if (quiet) {
+        rgb.gray('• ')
+      }
+      else {
+        rgb.gray(`    ${ result.test.name } `)
+        rgb.greenln( '✓' )
+      }
+    }
+    else if (result.failed) {
+      if (tap) {
+        console.log(`not ok ${ result.count } ${ result.test.name }`)
+      }
+      else if (quiet) {
+        rgb.red(`\n\n! ${ result.test.name } \n\n`)
+      }
+      else {
+        rgb.gray(`    ${ result.test.name } `)
+        rgb.redln( '✘' )
+        console.log('')
+      }
+
+      if (tap) {
+        console.log((result.e.stack ? result.e.stack.toString() : result.e.toString()).split('\n').map(line => `  ${ line }`).join( '\n'))
+      }
+      else {
+        prettyError(result.e)
+      }
+    }
   }
 
   return self
-
 }
-
 
 function prettyError(e) {
   const msg = e.stack
@@ -85,4 +144,5 @@ function prettyError(e) {
   const i = msg.indexOf('\n')
   rgb.yellowln(msg.slice(0, i))
   rgb.gray(msg.slice(i))
+  console.log('\n\n')
 }
